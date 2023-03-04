040b72cd4693470bd3baea79a2639e01b2a2ef88
Raghu Ganapathy <rgana001@ucr.edu>
2023-03-04 12:32:31 -0800

    broke the logic to parse json for coverage info into a function

--- scripts/tcas/tcasTester.py
+++ scripts/tcas/tcasTester.py
@@ -42,9 +42,8 @@ class TCASTESTER:
         # Loop through each testcase and run it
         for i, testcase in enumerate(testcases):
           
-           
             
-            coverageInfo = {}
+            coverageMeta = {}
 
             subprocess.run(["gcc-12", "-Wno-return-type", "-fprofile-arcs", "-ftest-coverage", "-w", "-g", "-o", "tcas", self.path + "tcas.c"])
 
@@ -59,15 +58,15 @@ class TCASTESTER:
 
             result = subprocess.run(["./tcas"] + agrsTest, stdout=subprocess.PIPE)
 
-            coverageInfo["testcaseID"] = i+1
-            coverageInfo["testcase"] = testcase
-            coverageInfo["TrueResult"] = result.stdout.decode('utf-8').strip()
+            coverageMeta["testcaseID"] = i+1
+            coverageMeta["testcase"] = testcase
+            coverageMeta["TrueResult"] = result.stdout.decode('utf-8').strip()
 
 
             # Run gcov on the output binary
 
             gcovRes = subprocess.run(["gcov-12","-a","-w","-b","-f", "-j", "tcas"], stdout=subprocess.PIPE)
-            # print(gcovRes.stdout.decode('utf-8'))
+
 
             
 
@@ -77,56 +76,9 @@ class TCASTESTER:
                 json_str = json_bytes.decode('utf-8')            
                 json_data = json.loads(json_str)     
 
+                coverageInfo = self.parseJsonDataForCoverage(json_data)
 
-                # For Coverage Ratio - Block Coverage
-                lines = json_data['files'][0]['lines']
-                covered_lines = [line for line in lines if line['count'] > 0]
-                total_lines = len(lines)
-                covered_ratio = len(covered_lines) / total_lines
-
-                #
-                coverageInfo["blockCoverage"] = covered_ratio
-
-                # To do the additional Coverage part
-                mp = {
-                    "visited" : [],
-                    "notVisited" : []
-                }
-                total_branches = 0
-                taken_branches = 0
-                for line in lines:
-                    if line["count"] > 0:
-                        mp["visited"].append(line["line_number"])
-                    else:
-                        mp["notVisited"].append(line["line_number"])
-                    
-                    if "branches" in line:
-                        # Loop through each branch in the line
-                        for branch in line["branches"]:
-                            # Increment the total number of branches
-                            total_branches += 1
-                            # If the branch was taken, increment the taken branches
-                            if branch["count"] > 0:
-                                taken_branches += 1
-                if total_branches > 0:
-                    branch_coverage = (taken_branches / total_branches) * 100
-                else:
-                    branch_coverage = 0.0
-
-                coverageInfo["lines"] = mp
-
-
-                # Per-Function Coverage
-                fnMp = {}
-                for function in json_data['files'][0]['functions']:
-
-                    fnBlockCoverage = function["blocks_executed"] / function["blocks"] 
-                    fnMp[function["name"]] = fnBlockCoverage
-                    # print("\t",function)
-                coverageInfo["functionData"] = fnMp
-
-
-
+                coverageInfo.update(coverageMeta)
 
                 self.coverageData.append(coverageInfo)
             os.system(f'rm -rf tcas.gcno')
@@ -143,6 +95,56 @@ class TCASTESTER:
         #     writer = csv.writer(csvfile)
         #     writer.writerows(coverage_info)
         
+    def parseJsonDataForCoverage(self, json_data):
+        coverageInfo = {}
+        lines = json_data['files'][0]['lines']
+        covered_lines = [line for line in lines if line['count'] > 0]
+        total_lines = len(lines)
+        covered_ratio = len(covered_lines) / total_lines
+
+        #
+        coverageInfo["blockCoverage"] = covered_ratio
+
+        # To do the additional Coverage part
+        mp = {
+            "visited" : [],
+            "notVisited" : []
+        }
+        total_branches = 0
+        taken_branches = 0
+        for line in lines:
+            if line["count"] > 0:
+                mp["visited"].append(line["line_number"])
+            else:
+                mp["notVisited"].append(line["line_number"])
+            
+            if "branches" in line:
+                # Loop through each branch in the line
+                for branch in line["branches"]:
+                    # Increment the total number of branches
+                    total_branches += 1
+                    # If the branch was taken, increment the taken branches
+                    if branch["count"] > 0:
+                        taken_branches += 1
+        if total_branches > 0:
+            branch_coverage = (taken_branches / total_branches) * 100
+        else:
+            branch_coverage = 0.0
+
+        coverageInfo["lines"] = mp
+
+
+        # Per-Function Coverage
+        fnMp = {}
+        for function in json_data['files'][0]['functions']:
+
+            fnBlockCoverage = function["blocks_executed"] / function["blocks"] 
+            fnMp[function["name"]] = fnBlockCoverage
+            # print("\t",function)
+        coverageInfo["functionData"] = fnMp
+
+        return coverageInfo
+
     def getCoverageData(self):
         print("TCASTESTER: getCoverageData")
         return self.coverageData
